c114e1ab13b2c9b70c561911b110aa976caf1916
Ong <ong2323@gmail.com>
2023-05-28 13:07:38 -0400

    time range lookup

--- src/app/api/restaurant/[slug]/availability/route.ts
+++ src/app/api/restaurant/[slug]/availability/route.ts
@@ -1,4 +1,6 @@
 import { NextResponse, NextRequest } from "next/server";
+import { times } from "../../../../../data";
+
 type SlugParam = {
   params: {
     slug: string;
@@ -19,7 +21,18 @@ export async function GET(req: NextRequest, { params }: SlugParam) {
     );
   }
 
+  const searchTimes = times.find((t) => {
+    return t.time === time;
+  })?.searchTimes;
+
+  if (!searchTimes) {
+    return NextResponse.json(
+      { errorMessage: "Please provide a valid time" },
+      { status: 400 }
+    );
+  }
+
   return NextResponse.json({ slug, day, time, partySize });
 }
 
-//http://localhost:3000/api/restaurant/kamasutra-indian-restaurant-and-wine-bar-niagara/availibility
+//http://localhost:3000/api/restaurant/kamasutra-indian-restaurant-and-wine-bar-niagara/availability?day=2023-01-01&time=20:00:00.000Z&partySize=4
