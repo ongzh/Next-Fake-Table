96b5f1b8c8c8d826539c228f84d03cf967be3266
Ong <ong2323@gmail.com>
2023-06-01 23:09:01 -0400

    reservation front end display

--- src/app/reserve/[slug]/page.tsx
+++ src/app/reserve/[slug]/page.tsx
@@ -1,11 +1,41 @@
 import Header from "./components/Header";
 import Form from "./components/Form";
+import { notFound } from "next/navigation";
+import { PrismaClient } from "@prisma/client";
 
-export default function Reserve() {
+const prisma = new PrismaClient();
+
+const fetchRestaurantBySlug = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+  });
+  if (!restaurant) {
+    notFound();
+  }
+  return restaurant;
+};
+
+export default async function Reserve({
+  params,
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams: { date: string; partySize: string; time: string };
+}) {
+  const restaurant = await fetchRestaurantBySlug(params.slug);
+  console.log(restaurant);
+
   return (
     <div className="border-t h-screen">
       <div className="py-9 w-3/5 m-auto">
-        <Header />
+        <Header
+          image={restaurant.main_image}
+          name={restaurant.name}
+          date={searchParams.date}
+          partySize={searchParams.partySize}
+        />
         <Form />
       </div>
     </div>
